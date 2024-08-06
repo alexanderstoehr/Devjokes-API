c4454e3d5c07ca44ac82bde6c24e2470170d5c35
Alexander Stöhr <git@alexander-stoehr.de>
2024-08-06 18:20:55 +0200

    added dummyjokes

---
+++ frontend/devjokes-api/src/common/dummyData.js
@@ -0,0 +1,105 @@
+const jokes =
+[
+    {
+        "id": 1,
+        "question": "Why did the noSQL developer leave the bar early?",
+        "punchline": "Because he did not find a table."
+    },
+    {
+        "id": 2,
+        "question": "",
+        "punchline": "My regex joke was so complex, even I didn’t know why it was funny."
+    },
+    {
+        "id": 3,
+        "question": "Why do programmers prefer dark mode?",
+        "punchline": "Because light attracts bugs."
+    },
+    {
+        "id": 4,
+        "question": "Why do Java developers wear glasses?",
+        "punchline": "Because they can't C#."
+    },
+    {
+        "id": 5,
+        "question": "How many programmers does it take to change a light bulb?",
+        "punchline": "None, that's a hardware problem."
+    },
+    {
+        "id": 6,
+        "question": "Why was the JavaScript developer sad?",
+        "punchline": "Because he didn't know how to 'null' his feelings."
+    },
+    {
+        "id": 7,
+        "question": "",
+        "punchline": "Programming is like writing a book... except if you miss a single comma on page 126 the whole thing makes no sense."
+    },
+    {
+        "id": 8,
+        "question": "What's a programmer's favorite hangout place?",
+        "punchline": "Foo Bar."
+    },
+    {
+        "id": 9,
+        "question": "Why did the developer go broke?",
+        "punchline": "Because he used up all his cache."
+    },
+    {
+        "id": 10,
+        "question": "Why do programmers hate nature?",
+        "punchline": "It has too many bugs."
+    },
+    {
+        "id": 11,
+        "question": "",
+        "punchline": "There are 10 types of people in the world: those who understand binary, and those who don’t."
+    },
+    {
+        "id": 12,
+        "question": "Why was the computer cold?",
+        "punchline": "It left its Windows open."
+    },
+    {
+        "id": 13,
+        "question": "How does a programmer go on a date?",
+        "punchline": "He writes a function."
+    },
+    {
+        "id": 14,
+        "question": "Why did the developer constantly fail at sports?",
+        "punchline": "Because he kept throwing exceptions."
+    },
+    {
+        "id": 15,
+        "question": "Why did the developer get stuck in the shower?",
+        "punchline": "Because the instructions on the shampoo bottle said: Lather, Rinse, Repeat."
+    },
+    {
+        "id": 16,
+        "question": "Why don't programmers like to walk?",
+        "punchline": "Because they have a hard time dealing with arrays."
+    },
+    {
+        "id": 17,
+        "question": "Why did the developer stay calm during the fire?",
+        "punchline": "Because he had caught the exception."
+    },
+    {
+        "id": 18,
+        "question": "How did the developer feel about his job?",
+        "punchline": "He was in a constant state of arrays."
+    },
+    {
+        "id": 19,
+        "question": "Why was the function sad?",
+        "punchline": "It didn't get the right arguments."
+    },
+    {
+        "id": 20,
+        "question": "",
+        "punchline": "Real programmers count from 0."
+    }
+]
+
+export default jokes;
