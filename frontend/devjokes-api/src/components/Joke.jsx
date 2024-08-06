4d93cc8c2d9137f375cc6f2a09fbff8677d89c9e
Alexander Stöhr <git@alexander-stoehr.de>
2024-08-06 18:01:55 +0200

    added joke and footer components

---
+++ frontend/devjokes-api/src/components/Joke.jsx
@@ -0,0 +1,10 @@
+export default function Joke({jokes}) {
+    let randomJoke = jokes[Math.floor(Math.random() * jokes.length)];
+    console.log(randomJoke)
+
+    return (
+        <>
+            <h1>Here's a joke</h1>
+        </>
+    );
+}
