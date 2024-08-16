57484dbbc128f140af6d092322c6a496d25d26f1
Alexander Stöhr <git@alexander-stoehr.de>
2024-08-16 17:36:04 +0200

    added joke card styling

--- frontend/devjokes-api/src/components/Joke.jsx
+++ frontend/devjokes-api/src/components/Joke.jsx
@@ -6,8 +6,10 @@ export default function Joke({jokes}) {
 
     return (
         <JokeCardStyled>
-            <div className="question">Here's a joke</div>
-            <div className="punchline">And a punchline</div>
+            <div className="joke">
+                <div className="question">Why did the nosql dev leave the bar early?</div>
+                <div className="punchline">Because he did not find a table.</div>
+            </div>
             <img src="/4chan-jokeface.svg"/>
         </JokeCardStyled>
     );
