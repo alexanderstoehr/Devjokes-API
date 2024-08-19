210ddb02c176b6e4d254785309ccd19214bd1609
Alexander Stöhr <git@alexander-stoehr.de>
2024-08-19 13:28:53 +0200

    added custom user model

---
+++ backend/user/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+User = get_user_model()
+
+# Register your models here.
+admin.site.register(User, UserAdmin)
