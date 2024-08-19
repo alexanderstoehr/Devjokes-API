88313de2652b4b7cc1bd3fc977caeec4549344df
Alexander Stöhr <git@alexander-stoehr.de>
2024-08-19 12:49:52 +0200

    added joke model

---
+++ backend/joke/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class Joke (models.Model):
+    question = models.CharField(max_length=1000, null=True, blank=True)
+    punchline = models.CharField(max_length=1000)
+    author = models.ForeignKey(to=User, related_name='jokes', on_delete=models.CASCADE, default=1)
+    likes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.question} - {self.punchline} by {self.author}"
