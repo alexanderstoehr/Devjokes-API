0701bf1d9153600f51ded6f7b9793f44f60fe013
Alexander Stöhr <git@alexander-stoehr.de>
2024-08-22 10:24:07 +0200

    added endpoints and descriptions in tabData object

--- frontend/devjokes-api/src/common/tabData.js
+++ frontend/devjokes-api/src/common/tabData.js
@@ -1,5 +1,6 @@
 const tabData = [
     {
+        id: 1,
         title: "Get all Jokes",
         description: "Get all jokes from the database",
         endpoint: "/joke/",
@@ -14,6 +15,85 @@ const tabData = [
             &emsp;&emsp;"question": "",<br>
             &emsp;&emsp;"punchline": "My regex joke was so complex, even I didn’t know why it was funny."<br>
             }]
+            `
+
+
+    },
+    {
+        id: 2,
+        title: "Get a single Joke",
+        description: "Get a specific joke by ID",
+        endpoint: "/joke/{ID}",
+        example: "/joke/1/",
+        content:
+            `
+            [{<br>
+            &emsp;&emsp;"id": 1,<br>
+            &emsp;&emsp;"question": "Why did the noSQL developer leave the bar early?",<br>
+            &emsp;&emsp;"punchline": "Because he did not find a table."<br>
+            }]
+            `
+
+
+    },
+    {
+        id: 3,
+        title: "Delete a Joke",
+        description: "Delete a specific joke by ID",
+        endpoint: "/joke/{ID}",
+        example: "/joke/1/",
+        content:
+            `
+            [{<br>
+            {"message": "Joke deleted successfully. One thing less to laugh about."}<br>
+            }]
+            `
+
+
+    },
+
+    {
+        id: 4,
+        title: "Post a Joke",
+        description: "Add a Joke to the database",
+        endpoint: "/joke/",
+        content:
+            `
+            [{<br>
+            &emsp;&emsp;"question": "Why did the noSQL developer leave the bar early?",<br>
+            &emsp;&emsp;"punchline": "Because he did not find a table."<br>
+            &emsp;}]
+            `
+
+
+    },
+    {
+        id: 5,
+        title: "Patch a Joke",
+        description: "Partial update of a Joke in the database",
+        endpoint: "/joke/",
+        content:
+            `
+            [{<br>
+            &emsp;&emsp;"question": "Why did the noSQL DEV leave the bar early?",<br>
+            &emsp;}]
+            `
+
+
+    },
+    {
+        id: 6,
+        title: "Search for Jokes",
+        description: "Get all jokes from the database, containing a string in question or punchline",
+        endpoint: "/joke/search/{search_term}",
+        example: "/joke/search/dev/",
+        content:
+            `
+            [{<br>
+            &emsp;&emsp;"id": 1,<br>
+            &emsp;&emsp;"question": "Why did the noSQL developer leave the bar early?",<br>
+            &emsp;&emsp;"punchline": "Because he did not find a table."<br>
+            &emsp;}]
             `
 
 
