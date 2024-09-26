0fd9fdf4f4044ba90504cc3f64b8b0269450fb55
Alexander Stöhr <git@alexander-stoehr.de>
2024-09-26 22:10:33 +0200

    fixed header nav targets

--- frontend/devjokes-api/src/components/sections/Header.jsx
+++ frontend/devjokes-api/src/components/sections/Header.jsx
@@ -7,9 +7,9 @@ export default function Header() {
                 <a className="grower" href="/devjokes-api/public"> <img src="/logo-as.svg" alt="DevJokes Logo"/></a>
                 <nav>
                     <ul>
-                        <li><a href="/devjokes-api/public#jokes">RANDOM JOKE</a></li>
-                        <li><a href="/devjokes-api/public#memes">CRUD A JOKE</a></li>
-                        <li><a href="/devjokes-api/public#videos">JOKE IN A PROJECT</a></li>
+                        <li><a href="#jokes">RANDOM JOKE</a></li>
+                        <li><a href="#memes">CRUD A JOKE</a></li>
+                        <li><a href="#videos">JOKE IN A PROJECT</a></li>
                     </ul>
                 </nav>
             </div>
