f7de1ffeba9b77b576f154d62af3e37c4aadac38
Alexander Stöhr <git@alexander-stoehr.de>
2024-09-30 21:32:52 +0200

    finished mobile navigation

--- frontend/devjokes-api/src/components/tabs/MobileTabNav.jsx
+++ frontend/devjokes-api/src/components/tabs/MobileTabNav.jsx
@@ -1,22 +1,23 @@
 import tabData from "../../common/tabData.js";
 import {useState} from "react";
 import {MobileTabNavStyled} from "../../styles/globalStyles.js";
+import {Navigate, NavLink, useNavigate} from "react-router-dom";
 
 
 export default function MobileTabNav() {
     const mobileNavItems = tabData
     const [currentTab, setCurrentTab] = useState(0)
+    const navigate = useNavigate()
 
     const navigateTabs = (move) => {
-        if (currentTab + move < 0) {
-            setCurrentTab(mobileNavItems.length - 1)
-            return
+        let newTab = currentTab + move
+        if (newTab < 0) {
+            newTab = mobileNavItems.length - 1
+        } else if (newTab >= mobileNavItems.length) {
+            newTab = 0
         }
-        if (currentTab + move >= mobileNavItems.length) {
-            setCurrentTab(0)
-            return
-        }
-        setCurrentTab(currentTab + move)
+        setCurrentTab(newTab)
+        navigate(`/${mobileNavItems[newTab].id}`)
     }
 
     return (
