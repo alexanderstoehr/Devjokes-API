125bb054c1e3ae841844a825ccac09727db1fbef
Alexander Stöhr <git@alexander-stoehr.de>
2024-10-04 20:50:03 +0200

    fixed api baseUrl

--- frontend/devjokes-api/src/common/api.js
+++ frontend/devjokes-api/src/common/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseUrl = "https://devjokes.com/api";
+export const baseUrl = "https://devjokes.alexander-stoehr.de/api";
 
 export const api = axios.create({
     baseURL: baseUrl,
