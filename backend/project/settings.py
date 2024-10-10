4947509ac323b17598b763a3e10b8e4c74630280
Alexander Stöhr <git@alexander-stoehr.de>
2024-10-10 19:33:47 +0200

    added cors headers

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     # 3rd Party
     "rest_framework",
     "drf_yasg",
+    "corsheaders",
     ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -138,3 +140,5 @@ REST_FRAMEWORK = {
         'user': '2/second'
         }
     }
+
+CORS_ALLOWED_ORIGINS = ['http://localhost:5174', 'https://devjokes.alexander-stoehr.de']
