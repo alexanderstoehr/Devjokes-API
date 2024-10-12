f11d500129037cc6e6fe0ba5fd2f807e23ec0e63
Alexander Stöhr <git@alexander-stoehr.de>
2024-10-12 18:54:09 +0200

    added django secret key env

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-in85!)e*^5a9hj*w^7483f8h)$)sdr(iye6w%#vu8&p79o&gm('
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
